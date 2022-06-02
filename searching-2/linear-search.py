6b3bc5b19311a6a55368177ac1fb20a8e73315d1
Vinit Raut <vinit.coder@gmail.com>
2022-06-03 02:00:13 +0530

    Add linear search

---
+++ searching-2/linear-search.py
@@ -0,0 +1,16 @@
+my_list = [4,5,9,0,1]
+
+ele = 7
+
+flag = false
+
+for i in my_list:
+  if(i == el):
+    flag = true
+    break
+
+if(flag == True):
+  print("Element found")
+
+else:
+  print("Element not found")
